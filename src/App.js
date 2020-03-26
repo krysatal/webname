0d914f503cd5a505e33eaf264e0c0b9bee4d8434
wje <892738614@qq.com>
2020-03-26 20:09:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import {Button, DatePicker, version} from 'antd'
+import 'antd/dist/antd.css'
+import './App.css'
+import axios from 'axios'
+// import setupProxy from './setupProxy'
+class App extends Component{
+    componentDidMount() {
+        axios.get('/api/mock/5e7c707aa123277163210e09/example/query').then((res) => {
+            console.log(res)
+        }).catch((res) => {
+            console.log(res)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>antd version: {version}</h1>
+                <DatePicker />
+                <Button type="primary" style={{ marginLeft: 8 }}>
+                    Primary Button
+                </Button>
+            </div>
+        );
+    }
 }
-
+// setupProxy(App)
 export default App;
---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware')
+module.exports = function (app) {
+    console.log(proxy)
+    console.log(app)
+    app.use(proxy.createProxyMiddleware('/api', {
+        target: 'https://www.easy-mock.com',
+        changeOrigin: true
+    }))
+}
